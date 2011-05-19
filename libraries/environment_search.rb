ec8a6c4270b4dc1a202eda8f4a92be126124bf09
Jesse Newland <jesse@jnewland.com>
2011-05-19 17:14:06 -0400

    update to be more like search function

--- libraries/environment_search.rb
+++ libraries/environment_search.rb
@@ -1,18 +1,22 @@
-# from twitter.com/mitchellh https://gist.github.com/8246b9361b49802d5915
+# originally from twitter.com/mitchellh https://gist.github.com/8246b9361b49802d5915
+# https://twitter.com/mitchellh/status/71318737235611649
 class Chef
     class Recipe
-    # This performs a node search on the chef server but restricts results
+    # This performs a search on the chef server but restricts results
     # to only nodes that are also in the same chef environment as this node.
     # This should be used for all searches as it maximizes the isolation
     # of each environment from each other.
-    def environment_search(query=nil, environment=nil, &block)
-      # Default the environment that is searched to our current environment
-      environment ||= node.chef_environment
 
-      # Default query to include all results
-      query ||= "chef:[* TO *]"
+    # All other arguments are passed on to Chef::Search::Query.new.search
+    def environment_search(*args, &block)
+      # extract the original query
+      query = args[1] || "*:*"
 
-      search(:node, "chef_environment:#{environment} AND (#{query})", &block)
+      # prepend the environment to the query
+      args[1] = "chef_environment:#{node.chef_environment} AND (#{query})"
+
+      # call the original search method
+      search(*args, &block)
     end
   end
 end
